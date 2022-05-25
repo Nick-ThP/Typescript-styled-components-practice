afde6b70c2e95494b18cd51c7150c83fd364fe54
Nicklas <nicklas_tp@hotmail.com>
2022-05-25 20:13:09 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const UserContext = createContext<string | null>(null)
+
+const UserPage: React.FC = () => {
+
+  const context = useContext(UserContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+      {context}
     </div>
+  )
+}
+
+const NewComponent: React.FC = () => {
+  const context = useContext(UserContext)
+
+  return (
+    <div style={{background: 'red'}}>
+      This is a new component
+      <br />
+      {context}
+    </div>
+  ) 
+}
+
+function App() {
+
+  const [user, setUser] = useState('Initial value')
+
+  const handleClick = () => {
+    setUser('Hello from button, ' + new Date().toDateString())
+  }
+
+  return (
+    <UserContext.Provider value={user}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello from homepage</h1>
+          <button onClick={handleClick}>Change value</button>
+          <UserPage />
+          <NewComponent />
+        </header>
+      </div>
+    </UserContext.Provider>
   );
 }
 
